0bfa539b48f8b15ed27d763d0d829c548c7bcc42
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2024-01-30 13:26:01 -0500

    Completed class assignment

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -1,8 +1,15 @@
 window.onload = function() {
     let x = document.getElementById('myBtn');
-    x.addEventListener('mouseover', myFunction('Moused over!'));
-    x.addEventListener('click', mySecondFunction('Clicked!'));
-    x.addEventListener('mouseout', myThirdFunction('Moused out!'));
+    x.addEventListener('mouseover', () => changeFunction('Moused over!'));
+    x.addEventListener('click', () => changeFunction('Clicked!'));
+    x.addEventListener('mouseout', () => changeFunction('Moused out!'));
+    // x.addEventListener('mouseover', myFunction('Moused over!'));
+    // x.addEventListener('click', mySecondFunction('Clicked!'));
+    // x.addEventListener('mouseout', myThirdFunction('Moused out!'));
+}
+
+function changeFunction(msg) {
+    document.getElementById('demo').textContent = msg;
 }
 
 function myFunction(msg) {
